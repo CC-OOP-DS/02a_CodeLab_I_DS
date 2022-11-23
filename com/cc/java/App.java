ba169e429e190318a0704a5b680e9d358a8012c3
Daniel <schindlerdaniel@web.de>
2022-11-23 13:05:34 +0100

    CodeChallenge

--- com/cc/java/App.java
+++ com/cc/java/App.java
@@ -1,13 +1,36 @@
 package com.cc.java;
 
+import java.lang.reflect.Array;
+
 public class App {
     
     public static void main(String[] args) {
-     
-    }
+        // Kontoeröffnung
+        Konto konto1 = new Konto (10000);
+        Konto konto2 = new Konto (10000);
+        Konto konto3 = new Konto (10000);
 
+        
+        // Output Kontostand 
+        output(String.valueOf(konto1.getKontostand()));
+        output(String.valueOf(konto2.getKontostand()));
+        output(String.valueOf(konto3.getKontostand()));
+
+        //Kontostand verändern
+        konto1.setKontostand(konto1.getKontostand()*2);
+        konto2.setKontostand(konto2.getKontostand()*3);
+        konto3.setKontostand(konto3.getKontostand()*10);
+
+        //Output neuer Kontostand
+        output(String.valueOf(konto1.getKontostand()));
+        output(String.valueOf(konto2.getKontostand()));
+        output(String.valueOf(konto3.getKontostand()));
+
+
+    }
+    // Wie bekomme ich "Kontostand von 'konto1': "
     public static void output(String outputStr) {
-        System.out.println(outputStr);
+        System.out.println("Kontostand: " + outputStr);
     }
 
 
